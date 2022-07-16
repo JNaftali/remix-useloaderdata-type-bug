939870af0caa681b9024e56d9736789056a361e4
JNaftali <marantzj@gmail.com>
2022-07-15 22:42:47 -0400

    reproduce problematic behavior

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,18 @@
+import { LoaderArgs, json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+export async function loader(_: LoaderArgs) {
+  return json({ example: ['a', 'b', 'c'] as ReadonlyArray<string> });
+}
+
 export default function Index() {
+  const { example } = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        <li>{example.join(', ')}</li>
       </ul>
     </div>
   );
